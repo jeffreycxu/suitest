5042062ff464fa88870fc74e2294996695907e1f
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-23 10:32:05 -0700

    adding wallet provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WalletKitProvider } from '@mysten/wallet-kit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <WalletKitProvider>
+      <App />
+    </WalletKitProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
